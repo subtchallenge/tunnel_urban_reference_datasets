6b2c0d68b7223007f5b9ab0a86c50bd34f61f85e
Arthur Schang <arthur.schang@parsons.com>
2020-02-20 17:38:46 -0500

    Updated build patch for Kimera-VIO.

--- apply_required_build_patches.sh
+++ apply_required_build_patches.sh
@@ -5,6 +5,8 @@ cd ../gflags_catkin
 git apply ../../../patches/gflagscatkin_build_patch.diff
 cd ../glog_catkin
 git apply ../../../patches/glogcatkin_build_patch.diff
+cd ../Kimera-VIO
+git apply ../../../patches/kimeravio_build_patch.diff
 cd ../opengv_catkin
 git apply ../../../patches/opengvcatkin_build_patch.diff
 cd ../../..
